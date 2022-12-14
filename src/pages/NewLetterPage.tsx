55a5dbfcc07ca7132871a3ebdbab2bc61604adea
jellyyams <53629007+jellyyams@users.noreply.github.com>
2022-12-14 17:10:55 -0500

    this is a test commit

--- src/pages/NewLetterPage.tsx
+++ src/pages/NewLetterPage.tsx
@@ -6,10 +6,7 @@ export default function NewLetterPage() {
     return (
         <Container className={styles.landingPage}>
             <Col md={8} className="mx-auto">
-                <h1 className={styles.title}>
-                    Create an open letter, sign an existing one, or edit an
-                    existing one
-                </h1>
+                <h1 className={styles.title}>This is a test commit</h1>
                 <Stack className="col-md-5 mx-auto" gap={2}>
                     <Button variant="primary">Create an Open Letter</Button>
                     <Button variant="outline-primary">
