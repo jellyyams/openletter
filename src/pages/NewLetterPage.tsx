a563ef26d6b8a606cf5d54799167429775a4f57b
jellyyams <53629007+jellyyams@users.noreply.github.com>
2022-12-14 19:09:23 -0500

    add routes

--- src/pages/NewLetterPage.tsx
+++ src/pages/NewLetterPage.tsx
@@ -5,18 +5,20 @@ import styles from './Pages.module.css'
 export default function NewLetterPage() {
     return (
         <Container className={styles.landingPage}>
-            <Col md={8} className="mx-auto">
-                <h1 className={styles.title}>This is a test commit</h1>
-                <Stack className="col-md-5 mx-auto" gap={2}>
-                    <Button variant="primary">Create an Open Letter</Button>
-                    <Button variant="outline-primary">
-                        Sign an Open Letter
-                    </Button>
-                    <Button variant="outline-primary">
-                        Edit an Open Letter
-                    </Button>
-                </Stack>
-            </Col>
+            <Form>
+                <Col md={12} className="mx-auto">
+                    <h1 className={styles.title}>Create a new open letter</h1>
+                    <Stack className="col-md-5 mx-auto" gap={2}>
+                        <Button variant="primary">Create an Open Letter</Button>
+                        <Button variant="outline-primary">
+                            Sign an Open Letter
+                        </Button>
+                        <Button variant="outline-primary">
+                            Edit an Open Letter
+                        </Button>
+                    </Stack>
+                </Col>
+            </Form>
         </Container>
     )
 }
